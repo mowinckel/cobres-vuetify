b59b74ef2b952dc48d09bc792d4e20c5bb235a50
mowinckel <ragnhildmowinckel@gmail.com>
2020-02-12 14:09:27 +0900

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,230 +1,145 @@
 <template>
-  <v-container>
-    <v-fab-transition>
-      <v-speed-dial
-        v-model="fab"
-        fixed
-        fab
-        large
-        dark
-        bottom
-        right
-        v-show="!hidden"
-      >
-        <template v-slot:activator>
-          <v-btn v-model="fab" color="green" dark fab>
-            <v-icon v-if="fab">mdi-close</v-icon>
-            <v-icon v-else>mdi-account-circle</v-icon>
-          </v-btn>
-        </template>
-        <v-btn fab dark small color="green" @click="dialog = true">
-          <v-icon>mdi-pencil</v-icon>
-        </v-btn>
-      </v-speed-dial>
-    </v-fab-transition>
-    <form @submit.prevent="create">
-      <v-text-field
-        v-model="$v.ip.$model"
-        solo
-        error-messages="error"
-        label="127.0.0.1"
-        @input="$v.ip.$touch()"
-        required
-        @blur="$v.ip.$touch()"
-      >
-      </v-text-field>
-    </form>
-
-    <h2 class="display-1 success--text pl-4">
-      Miners:
-      <v-fade-transition leave-absolute>
-        <span :key="`tasks-${miners.length}`">
-          {{ miners.length }}
-        </span>
-      </v-fade-transition>
-    </h2>
-
-    <v-divider class="mt-4"></v-divider>
-
-    <v-row class="my-1" align="center">
-      <strong class="mx-4 info--text text--darken-3">
-        Monitoring : {{ miners.length }}
-      </strong>
-
-      <v-divider vertical></v-divider>
-
-      <strong class="mx-4 black--text"> Down: {{ completedTasks }} </strong>
-
-      <v-spacer></v-spacer>
-    </v-row>
-
-    <v-divider class="mb-4"></v-divider>
-
-    <v-card v-if="miners.length > 0">
-      <!-- <v-list-item-group
-        v-model="selected"
-        multiple
-        active-class="green--text"
-        dense
-      > -->
-      <draggable v-model="miners" v-bind="dragOptions" @change="change">
-        <!-- <v-slide-y-transition class="py-0" group> -->
-        <transition-group type="transition" name="flip">
-          <v-list-item
-            three-line
-            v-for="(task, i) in miners"
-            :key="`${i}-${task}`"
+  <v-card tile flat>
+    <v-data-table
+      :headers="headers"
+      :items="miners"
+      v-model="selected"
+      show-select
+      single-select
+      item-key="ip"
+    >
+      <template v-slot:item.url="props">
+        <v-chip label class="pa-1">{{ props.item.summary.url }}</v-chip>
+      </template>
+      <template v-slot:item.summary.acc="props">
+        <v-chip outlined small color="green" class="pa-1">
+          {{ props.item.summary.acc }}
+        </v-chip>
+      </template>
+      <template v-slot:item.summary.lastacctime="props">
+        <v-chip small outlined color="green" class="pa-1">{{
+          props.item.summary.lastacctime
+        }}</v-chip>
+      </template>
+      <template v-slot:item.summary.rej="props">
+        <v-chip small outlined color="red accent-2" class="pa-1">
+          {{ props.item.summary.rej }}</v-chip
+        >
+      </template>
+      <template v-slot:item.summary.temp="props">
+        <v-chip small class="pa-1" outlined>
+          {{ props.item.summary.temp }} C
+        </v-chip>
+      </template>
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title class="display-1 font-weight-thin"
+            >Super Monitor</v-toolbar-title
           >
-            <template v-slot:default="{ active }">
-              <v-list-item-action>
-                <v-checkbox v-model="active" color="green"></v-checkbox>
-              </v-list-item-action>
 
-              <v-list-item-content>
-                <v-btn
-                  class="headline"
-                  :href="`http://${task.ip}`"
-                  depressed
-                  outlined
-                  rounded
-                  color="green"
-                  >{{ task.ip }}</v-btn
-                >
-                <v-text-field
-                  v-model="task.summary.url"
-                  hide-details
-                  dense
-                  rounded
-                  filled
-                ></v-text-field>
-                <v-text-field
-                  v-model="task.summary.user"
-                  hide-details
-                  dense
-                  rounded
-                  filled
-                ></v-text-field>
-              </v-list-item-content>
-
-              <v-list-item-action>
-                <v-list-item-action-text class="body-2"
-                  >temp : {{ task.summary.temp }}C</v-list-item-action-text
-                >
-                <v-list-item-action-text class="body-2"
-                  >uptime :
-                  {{ uptime(task.summary.uptime) }}</v-list-item-action-text
-                >
-                <v-list-item-action-text class="body-2"
-                  >last accept :
-                  {{
-                    timeago(task.summary.lastacctime)
-                  }}</v-list-item-action-text
-                >
-                <v-list-item-action-text class="body-2"
-                  >reject :{{ task.summary.rej }}</v-list-item-action-text
-                >
-                <v-list-item-action-text class="body-2"
-                  >algorithm :{{ task.summary.algo }}</v-list-item-action-text
-                >
-                <v-list-item-action-text class="body-2"
-                  >{{ task.summary.khs }}kH/s</v-list-item-action-text
-                >
-              </v-list-item-action>
+          <v-dialog v-model="dialog" width="800">
+            <template v-slot:activator="{ on }">
+              <v-btn color="green" dark v-on="on" outlined rounded>
+                Setting
+              </v-btn>
             </template>
-          </v-list-item>
-        </transition-group>
-        <!-- </v-slide-y-transition> -->
-      </draggable>
-      <!-- </v-list-item-group> -->
 
-      <v-dialog v-model="dialog" max-width="500" scrollable>
-        <v-card>
-          <v-alert prominent type="error" title tile>
-            <v-row align="center">
-              <v-col class="grow">Caution!! This is advanced setting</v-col>
-            </v-row>
-          </v-alert>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
 
-          <v-card-text style="height: 300px">
-            <v-col cols="12">
-              <v-text-field
-                v-for="[key, value] in Object.entries(dialogContent)"
-                :label="key"
-                :placeholder="value"
-                outlined
-                :key="key"
-              ></v-text-field>
-            </v-col>
-          </v-card-text>
+              <v-card-text>
+                <v-textarea hide-details outlined></v-textarea>
+              </v-card-text>
 
-          <v-divider></v-divider>
-          <v-card-actions>
-            <v-spacer></v-spacer>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green" text @click="save(selected[0].ip)"
+                  >Save</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
 
-            <v-btn color="green darken-1" text large @click="dialog = false">
-              Cancel
-            </v-btn>
+          <v-spacer></v-spacer>
+          <v-switch
+            prepend-icon="mdi-brightness-6"
+            v-model="$vuetify.theme.dark"
+            hide-details
+            inset
+            color="pink accent-2"
+          ></v-switch>
+        </v-toolbar>
+      </template>
+    </v-data-table>
 
-            <v-btn color="green darken-1" text large @click="dialog = false">
-              Apply
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-card>
-  </v-container>
+    <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
+      {{ snackText }}
+      <v-btn text @click="snack = false">Close</v-btn>
+    </v-snackbar>
+  </v-card>
 </template>
 
 <script>
 import { validationMixin } from "vuelidate";
 import { minLength, required } from "vuelidate/lib/validators";
-import draggable from "vuedraggable";
 import TimeAgo from "javascript-time-ago";
-import ko from "javascript-time-ago/locale/ko";
-TimeAgo.addLocale(ko);
+import en from "javascript-time-ago/locale/en";
+TimeAgo.addLocale(en);
 
 export default {
   mixins: [validationMixin],
   validations: {
     ip: { required, minLength: minLength(8) }
   },
-  components: {
-    draggable
-  },
   beforeMount() {
-    // eslint-disable-next-line no-unused-vars
+    // this.$vuetify.theme.dark = fal; // eslint-disable-next-line no-unused-vars
     this.axios.get("/v1/api/miners").then(response => {
       this.miners = response.data;
+      this.miners.forEach(element => {
+        element.summary.lastacctime = this.timeago(element.summary.lastacctime);
+        element.summary.uptime = `${parseInt(element.summary.uptime / 3600)} h`;
+        element.summary.freq = `${parseInt(
+          element.summary.freq / 1000000
+        )} MHz`;
+      });
     });
   },
   methods: {
+    save(ip) {
+      // eslint-disable-next-line no-console
+      console.log(ip);
+      this.axios.put(`/v1/api/miners/status/${ip}`).then(() => {
+        this.snack = true;
+        this.snackColor = "success";
+        this.snackText = "Data saved";
+      });
+    },
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
     apply(ip, setting) {
-      this.axios
-        .put(`/v1/api/miners/status/${ip}`, { setting: setting })
-        .then(response => {
-          // eslint-disable-next-line no-console
-          console.log(response);
-        });
+      this.axios.put(`/v1/api/miners/status/${ip}`, setting).then(response => {
+        // eslint-disable-next-line no-console
+        console.log(response.data);
+      });
     },
     setting(content) {
       this.dialogContent = content;
       this.dialog = true;
     },
-    change() {
-      // this.axios.post("/v1/api/miner");
-      // this.miners.forEach((element, index) => {
-      // });
-    },
-    uptime(seconds) {
-      return `${parseInt(seconds / 3600)} 시간`;
-    },
     timeago(time) {
       const timeAgo = new TimeAgo("ko-KR");
       const result = timeAgo.format(new Date(time));
 
-      if (result === "지금") {
+      if (result === "just now") {
         // eslint-disable-next-line no-console
-        return `${Math.floor((new Date() - new Date(time)) / 1000)}초 전`;
+        return `${Math.floor((new Date() - new Date(time)) / 1000)}s ago`;
       } else {
         return result;
       }
@@ -238,11 +153,35 @@ export default {
     }
   },
   data: () => ({
-    test: false,
-    fab: false,
-    hidden: false,
-    dialogContent: [],
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
+    },
     dialog: false,
+    snack: false,
+    snackColor: "",
+    snackText: "",
+    max25chars: v => v.length <= 25 || "Input too long!",
+    pagination: {},
+    headers: [
+      { text: "url", value: "url" },
+      { text: "user", value: "summary.user" },
+      { text: "pass", value: "summary.pass", sortable: false },
+      { text: "acc", value: "summary.acc" },
+      { text: "acc/min", value: "summary.accmn" },
+      { text: "reject", value: "summary.rej" },
+      { text: "temp", value: "summary.temp" },
+      { text: "lastacc", value: "summary.lastacctime" },
+      { text: "uptime", value: "summary.uptime" },
+      { text: "hashrate", value: "summary.khs" },
+      { text: "algorithm", value: "summary.algo" },
+      { text: "netdiff", value: "summary.diff" },
+      { text: "pooldiff", value: "summary.stdiff" },
+      { text: "freq", value: "summary.freq" }
+    ],
     ip: "",
     rules: [
       value => {
@@ -251,10 +190,23 @@ export default {
       }
     ],
     selected: [],
-    miners: [],
+    miners: [
+      {
+        summary: {
+          url: "dummy",
+          temp: 43
+        }
+      }
+    ],
     loading: false,
     loader: null
   }),
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    }
+  },
+
   watch: {
     loader() {
       const l = this.loader;
@@ -263,52 +215,6 @@ export default {
 
       this.loader = null;
     }
-  },
-  computed: {
-    dragOptions() {
-      return {
-        animation: 200,
-        group: "description",
-        disabled: false,
-        ghostClass: "ghost"
-      };
-    },
-    completedTasks() {
-      return 0;
-      // return this.miners.filter(task => task.done).length;
-    },
-    progress() {
-      return (this.completedTasks / this.miners.length) * 100;
-    },
-    remainingTasks() {
-      return this.miners.length - this.completedTasks;
-    }
   }
 };
 </script>
-
-<style scoped>
-.ghost {
-  opacity: 0.4;
-  background: #38c3a4;
-}
-.flip-list-move {
-  transition: transform 0.5s;
-}
-.no-move {
-  transition: transform 0s;
-}
-.list-group {
-  min-height: 20px;
-}
-.list-group-item {
-  cursor: move;
-}
-.list-group-item i {
-  cursor: pointer;
-}
-.custom-loader {
-  animation: loader 1s infinite;
-  display: flex;
-}
-</style>
